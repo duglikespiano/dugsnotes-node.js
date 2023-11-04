310e3fecba6007e866d272de9347504632e84120
SUHO PARK <duglikespiano@gmail.com>
2023-11-04 15:16:21 +0900

    DAY3 - SSR WITH TEMPLATES(HANDLEBARS)

--- src/3_templates/server.ts
+++ src/3_templates/server.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
+import { engine } from 'express-handlebars';
 import adminRoutes from './routes/admin';
 import shopRoutes from './routes/shop';
 
 const app = express();
 
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
+
+app.engine(
+	'hbs',
+	engine({
+		extname: '.hbs',
+		partialsDir: path.join(__dirname, 'views/partials'),
+		layoutsDir: path.join(__dirname, 'views/layouts'),
+		defaultLayout: 'main-layout',
+	})
+);
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
